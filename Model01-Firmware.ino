f2d45051a6f2cb5ca1e2663d02920e7b23f976b1
fluffy <fluffy@beesbuzz.biz>
2017-10-29 20:21:16 -0700

    I literally never need numlock, but not having a sensible backslash key was driving me nuts

--- Model01-Firmware.ino
+++ Model01-Firmware.ino
@@ -134,7 +134,7 @@ const Key keymaps[][ROWS][COLS] PROGMEM = {
    Key_LeftShift, Key_Backspace, Key_LeftGui, Key_LeftControl,
    ShiftToLayer(FUNCTION),
 
-   M(MACRO_ANY),  Key_6, Key_7, Key_8,     Key_9,         Key_0,         Key_KeypadNumLock,
+   M(MACRO_ANY),  Key_6, Key_7, Key_8,     Key_9,         Key_0,         Key_Backslash,
    Key_Enter,     Key_Y, Key_U, Key_I,     Key_O,         Key_P,         Key_Equals,
                   Key_H, Key_J, Key_K,     Key_L,         Key_Semicolon, Key_Quote,
    Key_RightGui,  Key_N, Key_M, Key_Comma, Key_Period,    Key_Slash,     Key_Minus,
