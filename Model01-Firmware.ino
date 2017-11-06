ef88b56a9f8b2dbdfedc3198a6e0a3a91ecfcc0e
fluffy <fluffy@beesbuzz.biz>
2017-11-06 00:42:46 -0800

    Swap tab and backspace, right ctrl and right alt

--- Model01-Firmware.ino
+++ Model01-Firmware.ino
@@ -128,17 +128,17 @@ const Key keymaps[][ROWS][COLS] PROGMEM = {
 
   [QWERTY] = KEYMAP_STACKED
   (___,          Key_1, Key_2, Key_3, Key_4, Key_5, Key_LEDEffectNext,
-   Key_Backtick, Key_Q, Key_W, Key_E, Key_R, Key_T, Key_Tab,
+   Key_Backtick, Key_Q, Key_W, Key_E, Key_R, Key_T, Key_Backspace,
    Key_PageUp,   Key_A, Key_S, Key_D, Key_F, Key_G,
    Key_PageDown, Key_Z, Key_X, Key_C, Key_V, Key_B, Key_Escape,
-   Key_LeftShift, Key_Backspace, Key_LeftGui, Key_LeftControl,
+   Key_LeftShift, Key_Tab, Key_LeftGui, Key_LeftControl,
    ShiftToLayer(FUNCTION),
 
    M(MACRO_ANY),  Key_6, Key_7, Key_8,     Key_9,         Key_0,         Key_Backslash,
    Key_Enter,     Key_Y, Key_U, Key_I,     Key_O,         Key_P,         Key_Equals,
                   Key_H, Key_J, Key_K,     Key_L,         Key_Semicolon, Key_Quote,
    Key_RightGui,  Key_N, Key_M, Key_Comma, Key_Period,    Key_Slash,     Key_Minus,
-   Key_RightControl, Key_LeftAlt, Key_Spacebar, Key_RightShift,
+   Key_LeftAlt, Key_RightControl, Key_Spacebar, Key_RightShift,
    ShiftToLayer(FUNCTION)),
 
   [FUNCTION] =  KEYMAP_STACKED
@@ -276,7 +276,7 @@ void setup()
         &LEDControl,
 
         // We start with the LED effect that turns off all the LEDs.
-        &LEDOff,
+        // &LEDOff,
 
         // The rainbow effect changes the color of all of the keyboard's keys at the same time
         // running through all the colors of the rainbow.
@@ -340,7 +340,8 @@ void setup()
     // We want to make sure that the firmware starts with LED effects off
     // This avoids over-taxing devices that don't have a lot of power to share
     // with USB devices
-    LEDOff.activate();
+    //LEDOff.activate();
+    LEDRainbowWaveEffect.activate();
 }
 
 /** loop is the second of the standard Arduino sketch functions.
